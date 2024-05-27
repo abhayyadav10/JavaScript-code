c34f805ccce80d1232e17c8d2ff35dd868808bf7
abhayyadav10 <123543757+abhayyadav10@users.noreply.github.com>
2024-05-27 06:19:22 +0000

    first lecture

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=14454
+let accountEmail="abhay@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+let accountState;
+
+// accountId=2    //not allowed
+
+accountEmail="akhkjga@gmail.com"
+accountPassword="3434534"
+accountCity="Ranchi"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState]);
