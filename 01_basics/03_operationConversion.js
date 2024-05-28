f5743b3ec2ef4a2ab82af3cbf5e8f53446d4675b
abhayyadav10 <123543757+abhayyadav10@users.noreply.github.com>
2024-05-28 05:12:13 +0000

    conversion of datatypes

---
+++ 01_basics/03_operationConversion.js
@@ -0,0 +1,25 @@
+// let score="44abc"
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+let isLoggedIn=0
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+ 1 =>true; 0=>false
+"" =>false
+"hitesh"=> true
+*/
+
+let someNumber = 33;
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber); 
