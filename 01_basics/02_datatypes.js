2e5b3a78116d05499e0fdd6e4301267ced2924fc
abhayyadav10 <123543757+abhayyadav10@users.noreply.github.com>
2024-05-28 04:49:47 +0000

    datatypes and ecma standard

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+let name = "Abhay";
+let age=21;
+let isLoggedIn=false;
+let state;
+
+// number=>2 to power 53
+// bigint
+//string=>" "
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol
+ 
+console.log(typeof null);
